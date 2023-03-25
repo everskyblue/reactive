f8d9c7cec25a4010e21bbf1e166ceef0e2e8df8e
hicode <hicodx@gmail.com>
2023-03-25 15:29:49 -0500

    eliminacion de archivos y ruta del codigo movida

--- src/contracts/IComponentDOM.ts
+++
@@ -1,9 +0,0 @@
-import IComponent from "./IComponent";
-import { IGeneral } from "./IElement";
-
-interface IComponentDOM extends IComponent {
-    tagname: string
-    attrs: Record<string, any>
-}
-
-export default IComponentDOM;
--- test/views/services/urls.ts
+++
@@ -1,5 +0,0 @@
-export const urlJsonPlaceholder = {
-    posts: 'https://jsonplaceholder.typicode.com/posts',
-    comments: 'https://jsonplaceholder.typicode.com/comments',
-    users: 'https://jsonplaceholder.typicode.com/users'
-}
