6679f7bb819bbfc18ab03255f6a5ed1d7e88bfe8
hicode <hicodx@gmail.com>
2023-04-27 18:36:42 -0500

    clas listener para escuchar un evento cuando el valor de una propiedad del contexto compartido a cambiado

---
+++ core/src/listener.ts
@@ -0,0 +1,43 @@
+type fnEvent = (data: any, oldData: any) => any;
+
+export class MapListeners extends Map implements Pick<string, any> {
+    [key: string]: any;
+
+    constructor() {
+        super();
+    }
+
+    set(prop: any, listener: Listeners): this {
+        super.set(prop, listener);
+        if (!(prop in this)) {
+            Object.defineProperty(this, prop, {
+                set(newValue: any) {
+                    for (const fn of listener.getQueue()) {
+                        fn(newValue, listener.value);
+                    }
+                    listener.value = newValue;
+                },
+                get() {
+                    return listener;
+                },
+            });
+        }
+        return this;
+    }
+}
+
+export class Listeners implements Pick<string, any> {
+    [key: string]: any;
+
+    private stacks: fnEvent[] = [];
+
+    constructor(public value: any) {}
+
+    getQueue() {
+        return this.stacks;
+    }
+
+    addListener(fn: fnEvent) {
+        this.stacks.push(fn);
+    }
+}
