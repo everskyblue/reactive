1a349ab5b97869ea73207d7fc298760e38836f37
hicodx <hicodx@gmail.com>
2023-12-18 13:40:09 -0400

    se mejoro la logica y se corrigio un error de doble ejecucion de comprobacion de rutas, se añadio hooks donde obtienen los parametros y el path de la ruta

--- core/src/navigation.ts
+++ core/src/navigation.ts
@@ -5,47 +5,44 @@ import type {
 import { exec } from "./hooks";
 import { useState } from "./hooks/useState";
 
-
 interface RouteProps {
     path: string;
     render: TreeWidget<any>;
 }
 
-function hashChange(routes: RouteProps[]) {
-    for (const route of routes) {
-        const hash =
-            location.hash.length && location.hash.startsWith("#")
-                ? location.hash.slice(1)
-                : "/";
-        if (route.path === hash) return route.render;
-    }
+let capture: {path: string, params: {[key: string]: string}};
+
+const component = (children: TreeWidget[]) => children.find(child => findRoute(child.properties as any));
+
+function findRoute({ path }: RouteProps) {
+    const hash =
+        location.hash.length && location.hash.startsWith("#")
+            ? location.hash.slice(1)
+            : "/";
+    const pattern = new globalThis.URLPattern(path, location.origin);
+    const match = pattern.exec(hash, location.origin);
+    capture = match ? {path: match.input, params: match.groups} : null;
+    return capture !== null;
 }
 
 export function Routes({
     children,
-    sharedContext
+    notFount,
 }: ReactivePropsWithChild<any>) {
-    const routes: RouteProps[] = children.map(
-        (child: TreeWidget<any>) =>
-            child.properties as unknown as RouteProps
-    );
-    const routeState = useState(hashChange(routes), this);
+    const invoke = useState(false, this);
+    const routeState = useState(!invoke.data ? component(children) : null ?? notFount, this);
     
-    if (sharedContext && !routeState.data.properties.sharedContext) {
-        routeState.data.properties.sharedContext = {};
-        for (const key in sharedContext) {
-            routeState.data.properties.sharedContext[key] = sharedContext[key];
-        }
-    }
-
     exec(() => {
         this.implementStates(routeState);
         window.addEventListener("hashchange", () => {
-            const route = hashChange(routes);
-            routeState.set(route ?? "page not fount");
-        }
-    )}, this)();
+            if (invoke.data === false) invoke.set(true);
+            routeState.set(component(children) ?? notFount);
+        })
+    }, this)();
 
+    if (!routeState.data.parentNode) {
+        routeState.data.parentNode = this;
+    }
 
     return routeState.data;
 }
@@ -53,4 +50,12 @@ export function Routes({
 export function Route(props: RouteProps) {
     props.render.parentNode = this;
     return props.render;
+}
+
+export function useParams() {
+    return capture.params;
+}
+
+export function usePath() {
+    return capture.path;
 }
